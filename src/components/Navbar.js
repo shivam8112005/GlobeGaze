f42ee54371634ccb71bc914a427dc340434d1382
shivam shukla <shivamshukla8112005@gmail.com>
2024-12-06 23:44:14 +0530

    fixed some url issue and made website for darkmode

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -16,7 +16,7 @@ export default class Navbar extends Component {
       <div>
         <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
   <div className="container-fluid">
-    <a className="navbar-brand" href="/">GlobeGaze</a>
+  <Link className="navbar-brand" to="/">GlobeGaze</Link>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>
--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -6,12 +6,12 @@ export default class NewsItem extends Component {
     let {title, description, imageUrl, newsUrl}=this.props;
     return (
       <div>
-        <div className="card my-3" style={{height: "25rem"}}>
+        <div className="card my-3" style={{height: "25rem", backgroundColor:'#212529', color:'white'}}>
   <img style={{height: "15rem"}} src={!imageUrl?"https://www.shutterstock.com/image-photo/newspaper-headline-business-news-260nw-262393247.jpg":imageUrl} className="card-img-top" alt="..."/>
   <div className="card-body">
     <h5 className="card-title">{title}</h5>
     <p className="card-text">{description}</p>
-    <a href={newsUrl} target="_blank" className="btn btn-dark btn-sm">Read More</a>
+    <a href={newsUrl} target="_blank" className="btn btn-dark btn-sm" style={{backgroundColor:'black'}}>Read More</a>
   </div>
 </div>
       </div>
